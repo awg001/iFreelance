e4c7dde15658dcac0d0ab6e5acfcba99193dcf2c
Deny <91406162+awg001@users.noreply.github.com>
2022-02-13 13:52:56 +0300

    django pro

---
+++ jobs/models.py
@@ -0,0 +1,18 @@
+from tabnanny import verbose
+from django.db import models
+
+
+class Job(models.Model):
+    url = models.URLField(unique=True)
+    title = models.CharField(max_length=250, verbose_name='Заказ')
+    price = models.CharField(max_length=50, verbose_name='Цена')
+    views = models.CharField(max_length=50, verbose_name='Просмотры')
+    respones = models.CharField(max_length=50, verbose_name='Отклики')
+    time = models.CharField(max_length=50, verbose_name='Время')
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
--- scrapping.py
+++ scrapping.py
@@ -1,30 +1,45 @@
+from random import choice
+
 import requests
 from bs4 import BeautifulSoup as BS
 
 
-url = 'https://freelance.habr.com/tasks?q=python'
+url = 'https://freelance.habr.com/tasks'
 
-res = requests.get(url)
-
-jobs = []
-
-if res.status_code == 200:
-    soup = BS(res.content, 'html.parser')
-    ul = soup.find('ul', id='tasks_list')
-    li = ul.find_all('li', class_='content-list__item')
-    for i in li:
-        url = 'https://freelance.habr.com/' + i.a['href']
-        title = i.a.text
-        price = 'Договорная'
-        if i.find('span', class_='count'):
-            price = i.find('span', class_='count').text  
-        views = i.find('span', class_='icon_task_views').text.strip() 
-        responses = i.find('span', class_='icon_task_responses').text
-        time = i.find('span', class_='icon_task_publish_at').text.strip()
-        jobs.append({'url':url, 'title': title, 'price': price,
-                    'views': views, 'responses': responses, 'time': time})
+headers = [
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 Firefox/47.0',
+        'Accept': 'text/html,appication/xhtml+xml;q=0.9,*/*;q=0.8'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.123 Safari/537.36',
+        'Accept': 'text/html,appication/xhtml+xml;q=0.9,*/*;q=0.8'},
+    {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; rv:53.0) Gecko/20100101 Firefox/53.0',
+        'Accept': 'text/html,appication/xhtml+xml;q=0.9,*/*;q=0.8'}
+]
 
 
+def habr_parsing(url):
+    jobs = []
+    res = requests.get(url, headers=choice(headers))
+    if res.status_code == 200:
+        soup = BS(res.content, 'html.parser')
+        ul = soup.find('ul', id='tasks_list')
+        li = ul.find_all('li', class_='content-list__item')
+        for i in li:
+            url = 'https://freelance.habr.com/' + i.a['href']
+            title = i.a.text
+            price = 'Договорная'
+            if i.find('span', class_='count'):
+                price = i.find('span', class_='count').text  
+            views = i.find('span', class_='icon_task_views').text.strip() 
+            responses = ''
+            if i.find('span', class_='icon_task_responses'):    
+                responses = i.find('span', class_='icon_task_responses').text.strip()
+            time = i.find('span', class_='icon_task_publish_at').text.strip()
+            jobs.append({'url': url, 'title': title, 'price': price,
+                        'views': views, 'responses': responses, 'time': time})
+
+    return jobs
+# with open('jobs.json', 'a', encoding='utf-8') as f:
+#     json.dump(jobs, f, indent=4, ensure_ascii=False)
 
         
         
